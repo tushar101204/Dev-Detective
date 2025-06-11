33a1f72fdec7fee8ad80bcba41567153af119a26
tushar101204 <22ucs219@lnmiit.ac.in>
2025-06-11 23:36:52 +0530

    Update local storage dark mode

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "
 const url = "https://api.github.com/users/";
 
 
-let darkMode = false;
 
 // Event Listeners
 btnsubmit.addEventListener("click", function () {
@@ -71,51 +70,41 @@ async function getUserData(gitUrl) {
 
 function updateProfile(data) {
   if (data.message !== "Not Found") {
-    noresults.style.display = "none";
-    function checkNull(param1, param2) {
-      if (param1 === "" || param1 === null) {
-        param2.style.opacity = 0.5;
-        param2.previousElementSibling.style.opacity = 0.5;
-        return false;
-      } else {
-        return true;
+      noresults.style.display = "none";
+      function checkNull(param1, param2) {
+        if (param1 === "" || param1 === null) {
+          param2.style.opacity = 0.5;
+          param2.previousElementSibling.style.opacity = 0.5;
+          return false;
+        } 
+        else {
+          return true;
+        }
       }
-    }
-    avatar.src = `${data.avatar_url}`;
-    userName.innerText = data.name === null ? data.login : data.name;
-    user.innerText = `@${data.login}`;
-    user.href = `${data.html_url}`;
-    datesegments = data.created_at.split("T").shift().split("-");
-    date.innerText = `Joined ${datesegments[2]} ${months[datesegments[1] - 1]} ${datesegments[0]}`;
-    bio.innerText = data.bio == null ? "This profile has no bio" : `${data.bio}`;
-    repos.innerText = `${data.public_repos}`;
-    followers.innerText = `${data.followers}`;
-    following.innerText = `${data.following}`;
-    user_location.innerText = checkNull(data.location, user_location) ? data.location : "Not Available";
-    page.innerText = checkNull(data.blog, page) ? data.blog : "Not Available";
-    page.href = checkNull(data.blog, page) ? data.blog : "#";
-    twitter.innerText = checkNull(data.twitter_username, twitter) ? data.twitter_username : "Not Available";
-    twitter.href = checkNull(data.twitter_username, twitter) ? `https://twitter.com/${data.twitter_username}` : "#";
-    company.innerText = checkNull(data.company, company) ? data.company : "Not Available";
-    searchbar.classList.toggle("active");
-    profilecontainer.classList.toggle("active");
-  } else {
+      avatar.src = `${data.avatar_url}`;
+      userName.innerText = data.name === null ? data.login : data.name;
+      user.innerText = `@${data.login}`;
+      user.href = `${data.html_url}`;
+      datesegments = data.created_at.split("T").shift().split("-");
+      date.innerText = `Joined ${datesegments[2]} ${months[datesegments[1] - 1]} ${datesegments[0]}`;
+      bio.innerText = data.bio == null ? "This profile has no bio" : `${data.bio}`;
+      repos.innerText = `${data.public_repos}`;
+      followers.innerText = `${data.followers}`;
+      following.innerText = `${data.following}`;
+      user_location.innerText = checkNull(data.location, user_location) ? data.location : "Not Available";
+      page.innerText = checkNull(data.blog, page) ? data.blog : "Not Available";
+      page.href = checkNull(data.blog, page) ? data.blog : "#";
+      twitter.innerText = checkNull(data.twitter_username, twitter) ? data.twitter_username : "Not Available";
+      twitter.href = checkNull(data.twitter_username, twitter) ? `https://twitter.com/${data.twitter_username}` : "#";
+      company.innerText = checkNull(data.company, company) ? data.company : "Not Available";
+      searchbar.classList.toggle("active");
+      profilecontainer.classList.toggle("active");
+  } 
+  else {
     noresults.style.display = "block";
   }
 }
 
-
-//dark mode default
-const prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-if (localStorage.getItem("dark-mode")) {
-  darkMode = localStorage.getItem("dark-mode");
-  darkModeProperties();
-} else {
-  localStorage.setItem("dark-mode", prefersDarkMode);
-  darkMode = prefersDarkMode;
-  lightModeProperties();
-}
 
 function darkModeProperties() {
   root.setProperty("--lm-bg", "#141D2F");
@@ -142,5 +131,21 @@ function lightModeProperties() {
   localStorage.setItem("dark-mode", false);
 }
 
+init();
+function init(){
+ getUserData(url + "tushar101204");
+ let darkMode = false;
 
-getUserData(url + "tushar101204");
+ const val = localStorage.getItem("dark-mode");
+ if(val === null){
+  localStorage.setItem("dark-mode", darkMode);
+  lightModeProperties();
+ }
+ else if(val == "true"){
+  darkModeProperties();
+ }
+ else{
+  lightModeProperties();
+ }
+
+}
