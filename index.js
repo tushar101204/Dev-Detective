40e03dcbd5df5702f1c0dac46f9246fa67e5ebc3
tushar101204 <22ucs219@lnmiit.ac.in>
2025-06-11 20:32:39 +0530

    Update async await func

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const searchbar = document.querySelector(".searchbar-container");
 const profilecontainer = document.querySelector(".profile-container");
 const root = document.documentElement.style;
 const get = (param) => document.getElementById(`${param}`);
-const url = "https://api.github.com/users/";
+
 const noresults = get("no-results");
 const btnmode = get("btn-mode");
 const modetext = get("mode-text");
@@ -14,7 +14,6 @@ const avatar = get("avatar");
 const userName = get("name");
 const user = get("user");
 const date = get("date");
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const bio = get("bio");
 const repos = get("repos");
 const followers = get("followers");
@@ -23,6 +22,11 @@ const user_location = get("location");
 const page = get("page");
 const twitter = get("twitter");
 const company = get("company");
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const url = "https://api.github.com/users/";
+
+
 let darkMode = false;
 
 // Event Listeners
@@ -32,16 +36,13 @@ btnsubmit.addEventListener("click", function () {
   }
 });
 
-input.addEventListener(
-  "keydown",
-  function (e) {
+input.addEventListener("keydown",function (e) {
     if (e.key == "Enter") {
       if (input.value !== "") {
         getUserData(url + input.value);
       }
     }
   },
-  false
 );
 
 input.addEventListener("input", function () {
@@ -57,17 +58,16 @@ btnmode.addEventListener("click", function () {
 });
 
 // Functions
-function getUserData(gitUrl) {
-  fetch(gitUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      updateProfile(data);
-    })
-    .catch((error) => {
-      throw error;
-    });
+async function getUserData(gitUrl) {
+  try {
+    const response = await fetch(gitUrl);
+    const data = await response.json();
+    updateProfile(data);
+  } catch (error) {
+    throw error;
+  }
 }
+
 
 function updateProfile(data) {
   if (data.message !== "Not Found") {
